50605b44483073561c9c3002bed9d526f514c291
Tsuyoshi Aizu <aizu@bascule.co.jp>
2015-09-03 11:58:33 +0900

    added route53.tf

---
+++ route53.tf
@@ -0,0 +1,15 @@
+resource "aws_route53_zone" "default" {
+  name = "example.co.jp"
+}
+
+resource "aws_route53_record" "default" {
+  zone_id = "${aws_route53_zone.default.zone_id}"
+  name = "drive.bascule.co.jp"
+  type = "A"
+
+  alias {
+    name = "${aws_elb.default.dns_name}"
+    zone_id = "${aws_elb.default.zone_id}"
+    evaluate_target_health = false
+  }
+}
