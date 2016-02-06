d81a219b663c49f5d9d97411ba493e95487ec949
Tsuyoshi Aizu <aizu@bascule.co.jp>
2016-02-07 01:42:15 +0900

    fixed s3_proxy.tf

--- s3_proxy.tf
+++ s3_proxy.tf
@@ -2,13 +2,17 @@ variable "s3_acl" {
   default = "private"
 }
 
+variable "s3_backet" {
+  default = "${var.project}-${var.environment}"
+}
+
 resource "aws_s3_bucket" "default" {
-    bucket = "${var.project}-${var.environment}"
+    bucket = "${var.s3_backet}"
     acl = "${var.s3_acl}"
 
     tags {
         Name = "${var.environment}"
     }
 
-    policy = "{\"Statement\":[{\"Effect\":\"Deny\",\"Principal\":\"*\",\"Action\":\"s3:*\",\"Resource\":\"arn:aws:s3:::${var.project}-${var.environment}/*\",\"Condition\":{\"NotIpAddress\":{\"aws:SourceIp\":\"${aws_eip.default.public_ip}\"}}}]}"
+    policy = "{\"Statement\":[{\"Effect\":\"Deny\",\"Principal\":\"*\",\"Action\":\"s3:*\",\"Resource\":\"arn:aws:s3:::${var.s3_backet}/*\",\"Condition\":{\"NotIpAddress\":{\"aws:SourceIp\":\"${aws_eip.default.public_ip}\"}}}]}"
 }
