1549e7aae876f57309fb8051e4cef199d37ff2b4
Tsuyoshi Aizu <aizu@bascule.co.jp>
2016-05-19 21:54:57 +0900

    fixed s3_proxy

--- s3_proxy.tf
+++ s3_proxy.tf
@@ -14,5 +14,5 @@ resource "aws_s3_bucket" "default" {
         Name = "${var.environment}"
     }
 
-    policy = "{\"Statement\":[{\"Effect\":\"Deny\",\"Principal\":\"*\",\"Action\":\"s3:GetObject\",\"Resource\":\"arn:aws:s3:::${var.s3_backet}/*\",\"Condition\":{\"NotIpAddress\":{\"aws:SourceIp\":\"${aws_eip.default.public_ip}\"}}}]}"
+    policy = "{\"Id\":\"Policy1463662030599\",\"Version\":\"2012-10-17\",\"Statement\":[{\"Sid\":\"Stmt1463661969875\",\"Action\":\"s3:*\",\"Effect\":\"Allow\",\"Resource\":\"arn:aws:s3:::${var.s3_backet}/*\",\"Principal\":\"*\"},{\"Sid\":\"Stmt1463662026920\",\"Action\":[\"s3:GetObject\"],\"Effect\":\"Deny\",\"Resource\":\"arn:aws:s3:::${var.s3_backet}/*\",\"Condition\":{\"NotIpAddress\":{\"aws:SourceIp\":\"${aws_eip.default.public_ip}\"}},\"Principal\":\"*\"}]}"
 }
